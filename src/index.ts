e5cdbae25ff8bc544cb495b3dc3630120fffe7bc
Junho Lee <julee@student.42.us.org>
2024-05-25 19:08:56 +0900

    update version

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-import '../dist/klinecharts-ui.css';
 import { registerOverlay } from '@basttyy/klinecharts'
 
 import overlays from './extension'
